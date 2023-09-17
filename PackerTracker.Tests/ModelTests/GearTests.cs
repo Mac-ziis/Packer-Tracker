a52dbc96590e57aa18277a57907f385256a18d69
Mac-ziis <macg286@gmail.com>
2023-09-17 14:19:28 -0700

    GearTests.cs, first test, red and green phase.

--- PackerTracker.Tests/ModelTests/GearTests.cs
+++ PackerTracker.Tests/ModelTests/GearTests.cs
@@ -6,79 +6,16 @@ using System;
 namespace PackerTracker.Tests
 {
   [TestClass]
-  public class GearTests : IDisposable
+  public class GearTests
   {
 
-    // public void Dispose()
-    // {
-    //   Item.ClearAll();
-    // }
-
-    // [TestMethod]
-    // public void ItemConstructor_CreatesInstanceOfItem_Item()
-    // {
-    //   Item newItem = new Item("test");
-    //   Assert.AreEqual(typeof(Item), newItem.GetType());
-    // }
-
-    // [TestMethod]
-    // public void GetDescription_ReturnsDescription_String()
-    // {
-    //   //Arrange
-    //   string description = "Walk the dog.";
-
-    //   //Act
-    //   Item newItem = new Item(description);
-    //   string result = newItem.Description;
-
-    //   //Assert
-    //   Assert.AreEqual(description, result);
-    // }
-
-    // [TestMethod]
-    // public void SetDescription_SetDescription_String()
-    // {
-    //   //Arrange
-    //   string description = "Walk the dog.";
-    //   Item newItem = new Item(description);
-
-    //   //Act
-    //   string updatedDescription = "Do the dishes";
-    //   newItem.Description = updatedDescription;
-    //   string result = newItem.Description;
-
-    //   //Assert
-    //   Assert.AreEqual(updatedDescription, result);
-    // }
-
-    // [TestMethod]
-    // public void GetAll_ReturnsEmptyList_ItemList()
-    // {
-    //   // Arrange
-    //   List<Item> newList = new List<Item> { };
-
-    //   // Act
-    //   List<Item> result = Item.GetAll();
-
-    //   // Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
-
-    // [TestMethod]
-    // public void GetAll_ReturnsItems_ItemList()
-    // {
-    //   //Arrange
-    //   string description01 = "Walk the dog";
-    //   string description02 = "Wash the dishes";
-    //   Item newItem1 = new Item(description01);
-    //   Item newItem2 = new Item(description02);
-    //   List<Item> newList = new List<Item> { newItem1, newItem2 };
-
-    //   //Act
-    //   List<Item> result = Item.GetAll();
-
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+    [TestMethod]
+    public void GearConstructor_CreatesInstanceOfGear_Gear()
+    {
+      string gearName = "Boots";
+      Gear newGear = new Gear(gearName);
+      // Assert.AreEqual(typeof(Gear), newGear.GetType());
+    }
+    
   }
 }
--- PackerTracker/Models/Gear.cs
+++ PackerTracker/Models/Gear.cs
@@ -1,32 +1,15 @@
 using System.Collections.Generic;
 
+
 namespace PackerTracker.Models
 {
   public class Gear
   {
-    // public string Description { get; set; }
-    // public int Id { get; }
-    // private static List<Item> _instances = new List<Item> { };
+    public string Description { get; set; }
 
-    // public Item(string description)
-    // {
-    //   Description = description;
-    //   _instances.Add(this);
-    //   Id = _instances.Count;
-    // }
-
-    // public static List<Item> GetAll()
-    // {
-    //   return _instances;
-    // }
-
-    // public static void ClearAll()
-    // {
-    //   _instances.Clear();
-    // }
-    // public static Item Find(int searchId)
-    // {
-    //   return _instances[searchId-1];
-    // }
+    public Gear(string description)
+    {
+        Description = description;
+    }
   }
 }
